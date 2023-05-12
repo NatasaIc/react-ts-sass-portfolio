8729e7896809ad6cf14e7d53feae43f9aea4722d
NatasaIc <iconomou.gmail.com>
2023-05-12 07:00:17 +0200

    cleanup

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -6,14 +6,14 @@ export default function About() {
     <section className="section-about" id="about">
     <div className="u-center-text  u-margin-bottom-medium">
       <h2 className="heading-secondary heading-about">
-        A litle somthing about me
+        A little somthing about me
       </h2>
     </div>
 
     <div className="row">
       <div className="col-1-of-2">
         <p className="paragraph paragraph-about cutoff-text">
-        I'm a web developer student at Medieinstitutet. The program lasts 2 years. 
+          I'm a web developer student at Medieinstitutet. The program lasts 2 years. 
          I will have knowledge in 
          HTML, CSS/SCSS, JavaScript, TypeScript, React, 
          Database, Git, Node.js and PHP.
@@ -22,7 +22,7 @@ export default function About() {
          My philosophy has always been,
          self-editing, non-stop learning and humility are 
          key to my practice.<br /><br />
-         I am currently applying for LIA internship for the following periods:<br />
+         I am applying for LIA internship for the following periods:<br />
          LIA 1: 2023-10-16 to 2023-12-24<br />
          LIA 2: 2024-02-05 to 2024-05-19<br /> <br />
          If you run a company or work for a company that is 
